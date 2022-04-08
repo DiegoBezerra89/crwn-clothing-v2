2ee4b00851c4b457a434a2b18bf43a7c000e8a9d
DiegoMartinsWebjump <diego.martins@webjump.com.br>
2022-04-08 11:13:15 -0300

    Adicionado sign in

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -41,4 +41,22 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   const userSnapshot = await getDoc(userDocRef);
   console.log(userSnapshot);
   console.log(userSnapshot.exists());
+	console.log("userAuth", userAuth);
+
+	if(!userSnapshot.exists()) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await setDoc(userDocRef, {
+				displayName,
+				email,
+				createdAt
+			});
+		} catch(error) {
+			console.log('error creating user', error.message);
+		}
+	}
+	return userDocRef;
+
 };
