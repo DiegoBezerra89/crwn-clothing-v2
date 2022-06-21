4775e7ee7177a9deffa8961f1d74e5499c2318f5
Diego Bezerra Martins <diegobmartins21@gmail.com>
2022-06-21 07:31:56 -0300

    Applied QuantityHandler component

--- src/components/quantity-handler/quantity-handler.component.jsx
+++ src/components/quantity-handler/quantity-handler.component.jsx
@@ -4,15 +4,22 @@ import { CartContext } from "../../contexts/cart.context";
 
 const QuantityHandler = ({ product }) => {
   const { quantity, id } = product;
-  const { addItemToCart, decrementItemFromCart } = useContext(CartContext);
+  const { addItemToCart, decrementItemFromCart, deleteItemFromCart } =
+    useContext(CartContext);
   const addProductToCard = () => addItemToCart(product);
   const decrementProductFromCart = () => decrementItemFromCart(product);
-
+  const deleteProductFromCart = () => deleteItemFromCart(product);
   return (
     <>
       <button onClick={addProductToCard}>+</button>
       <span>{quantity}</span>
-      <button onClick={decrementProductFromCart}>-</button>
+      <button
+        onClick={
+          quantity > 1 ? decrementProductFromCart : deleteProductFromCart
+        }
+      >
+        -
+      </button>
     </>
   );
 };
