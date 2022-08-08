4d450a6aaa1cbda81e8c2d6e42de18504b8cd99c
DiegoBezerra89 <diegobmartins21@gmail.com>
2022-08-08 07:02:28 -0300

    Commit test

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,10 +1,9 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import ProductCard from "../../components/product-card/product-card.component";
 import { CategoriesContext } from "../../contexts/categories.context";
 
 const CategoryPage = () => {
   const { categoryProducts } = useContext(CategoriesContext);
-
   return (
     <>
       <div className="products-container">
