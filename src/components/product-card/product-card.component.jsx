4317a012fa8d600a341808444b67bc136c6787e4
DiegoBezerra89 <diegobmartins21@gmail.com>
2022-08-10 08:57:05 -0300

    ProductCard styled

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,10 +1,17 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 
-import { CartContext } from '../../contexts/cart.context';
+import { CartContext } from "../../contexts/cart.context";
 
-import Button from '../button/button.component';
+import Button from "../button/button.component";
 
-import './product-card.styles.scss';
+import {
+  ProductCardContainer,
+  ProductImage,
+  FooterContainer,
+  Name,
+  Price,
+  AddToCartButton,
+} from "./product-card.styles";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
@@ -13,16 +20,16 @@ const ProductCard = ({ product }) => {
   const addProductToCart = () => addItemToCart(product);
 
   return (
-    <div className='product-card-container'>
-      <img src={imageUrl} alt={`${name}`} />
-      <div className='footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
-      </div>
-      <Button buttonType='inverted' onClick={addProductToCart}>
+    <ProductCardContainer>
+      <ProductImage src={imageUrl} alt={`${name}`} />
+      <FooterContainer>
+        <Name>{name}</Name>
+        <Price>{price}</Price>
+      </FooterContainer>
+      <AddToCartButton buttonType="inverted" onClick={addProductToCart}>
         Add to card
-      </Button>
-    </div>
+      </AddToCartButton>
+    </ProductCardContainer>
   );
 };
 
